174e28d97a8ff95cb3cd2f78d19486525695ad99
AdamDHines <adam.hines@qut.edu.au>
2026-02-23 11:06:02 +1000

    fix up

--- eventgem/inference-jetson.py
+++ eventgem/inference-jetson.py
@@ -410,7 +410,7 @@ def main():
                         )
                         final_scores = cand_dist_val - (inlier_counts * args.inlier_weight)
                         best_arg = np.argmin(final_scores)
-                        np.savez_compressed(f"{qry_feat_dir}/ref_feats_{frame_idx}.npz", final_scores.cpu().numpy())
+                        np.savez_compressed(f"{qry_feat_dir}/ref_feats_{frame_idx}.npz", q_desc_vit.cpu().numpy())
 
                 # End of processing for this frame, record total time
                 t_total = (time.perf_counter() - cpu0) * 1000.0
